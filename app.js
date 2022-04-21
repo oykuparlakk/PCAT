09bf74c481e624a4937fc15dd3a53d7b2cacc64c
oyku <oyku@DESKTOP-73QGJ2T>
2022-04-21 20:19:57 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const fn = function () {
-  console.log('Function working');
-};
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static('public'));
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
