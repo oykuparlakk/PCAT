9e5115a8ae160f6a5d8b7645b24a7bf85e3de78e
oyku <oyku@DESKTOP-73QGJ2T>
2022-04-21 21:16:13 +0300

    pages are edited

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
+
+//Template Engine
+app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
 
 const port = 3000;
 
+//ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
 });
 
 app.listen(port, () => {
