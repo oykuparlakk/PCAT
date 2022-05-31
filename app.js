b37e05cb7e5e8bf4e6add540d7c9a50ff930e5e9
oyku <oyku@DESKTOP-73QGJ2T>
2022-05-31 15:37:42 +0300

    düzeltmeler yapıldı

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const ejs = require('ejs');
 const path = require('path');
+const fs = require('fs');
+const req = require('express/lib/request');
+const Photo = require('./models/Photo');
 
 const app = express();
-
-
+//connect db
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //Template Engine
 app.set('view engine', 'ejs');
@@ -12,13 +20,27 @@ app.set('view engine', 'ejs');
 //middlewares
 
 app.use(express.static('public'));
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
 
 const port = 3000;
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({}).sort('-dateCreated');
+  res.render('index', {
+    photos,
+  });
+});
+
+app.get('/photos/:id', async (req, res) => {
+  //console.log(req.params.id);
+  //res.render('about');
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -31,6 +53,28 @@ app.get('/contact', (req, res) => {
 
 app.get('/add', (req, res) => {
   res.render('add');
+});
+
+//posts
+app.post('/photos', async (req, res) => {
+  //console.log(req.files.image);
+  //await Photo.create(req.body);
+  //res.redirect('/');
+
+  const uploadDir = '/public/uploads';
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+  let uploadedImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name;
+
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadedImage.name,
+    });
+    res.redirect('/');
+  });
 });
 
 app.listen(port, () => {
